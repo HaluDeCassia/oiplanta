fe3d7ede66e17660efc03b035a72dee1afbfcad5
Gabriel Luz and Priscila Rachevsky and Halú de Cássia <gabriel.luz+priscila.rachevsky+halu.decassia@thoughtworks.com>
2021-04-24 18:05:52 -0300

    feat: create environment button component

---
+++ src/components/EnvironmentButton/index.tsx
@@ -0,0 +1,56 @@
+import React from  'react'
+import { 
+  StyleSheet, 
+  Text
+} from 'react-native'
+import { RectButton, RectButtonProps} from 'react-native-gesture-handler'
+
+import colors from '../../styles/colors'
+import fonts from '../../styles/fonts'
+
+interface EnvironmentButtonProps extends RectButtonProps {
+  children: React.ReactNode;
+  active?: boolean
+}
+
+export default function EnvironmentButton({ active=false, children, ...rest }: EnvironmentButtonProps) {
+  return (
+    <RectButton 
+      style={[
+        styles.container,
+        active && styles.containerActive
+      ]}
+      {...rest}
+    >
+      <Text style={[
+        styles.text,
+        active && styles.textActive
+      ]}>
+        {children}
+      </Text>
+    </RectButton>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.shape,
+    height: 40,
+    width: 76,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 12,
+    marginRight: 6
+  },
+  containerActive: {
+    backgroundColor: colors.green_light
+  },
+  text: {
+    color: colors.heading,
+    fontFamily: fonts.text
+  },
+  textActive: {
+    fontFamily: fonts.heading,
+    color: colors.green_dark,
+  }
+})
